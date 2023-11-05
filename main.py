bee146a51e380dec0fc85ded6f16cc14ecf20d57
namgivu <namgivu@gmail.com>
2023-11-05 15:53:55 +0700

    rade toya03 cau3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #region import bailam_f
-from s00_bailam import hi as bailam_f
+from s00_bailam import greeting as bailam_f
 #endregion import bailam_f
 
 
@@ -8,10 +8,25 @@ from s02_chambai import chambai
 
 #region testkey_list
 testcase_list = [
-  {'input': {'name':'AI BTX'}, 'output':'Hi AI BTX',   'tc_name': 'tc0' },
-  {'input': ['HSU'],           'output':'Hi',          'tc_name': 'tc1' },
-  {'input': [None],            'output':'Hi',          'tc_name': 'tc2' },
-  {'input': [''],              'output':'Hi AI BTX',   'tc_name': 'tc3' },
+  {'input': {'hour_str':'6am'}, 'output':'Good morning!',     'tc_name': 'tc00'},
+
+  {'input': ['6am'],            'output':'Good morning!',     'tc_name': 'tc01'},
+  {'input': ['6 am'],           'output':'Good morning!',     'tc_name': 'tc02'},
+  {'input': ['6AM'],            'output':'Good morning!',     'tc_name': 'tc03'},
+  {'input': ['6 AM'],           'output':'Good morning!',     'tc_name': 'tc04'},
+
+  {'input': ['9pm'],            'output':'Good evening!',     'tc_name': 'tc05'},
+  {'input': ['0900pm'],         'output':'Good evening!',     'tc_name': 'tc06'},
+  {'input': ['09:00pm'],        'output':'Good evening!',     'tc_name': 'tc07'},
+  {'input': ['09:00 pm'],       'output':'Good evening!',     'tc_name': 'tc08'},
+  {'input': ['09:00 PM'],       'output':'Good evening!',     'tc_name': 'tc09'},
+
+  {'input': ['1pm'],            'output':'Good afternoon!',   'tc_name': 'tc10'},
+
+  {'input': ['06:00'],          'output':'Good morning!',   'tc_name': 'tc11'},
+  {'input': ['0600'],           'output':'Good morning!',   'tc_name': 'tc12'},
+  {'input': ['21:00'],          'output':'Good evening!',   'tc_name': 'tc13'},
+  {'input': ['2100'],           'output':'Good evening!',   'tc_name': 'tc14'},
 ]
 #endregion testkey_list
 
--- s00_bailam.py
+++ s00_bailam.py
@@ -1,39 +1,58 @@
 #region debai
 """
 --- ma debai / id
-hi(name)
+greeting(hour_str)
 
 --- nopbai
 00 fork tu bainopmau tren replit tu trang web duoiday 
-   https://replit.com/@NamG1/TODO
+   https://replit.com/@NamG1/toya03greetinghourstr
 
 01 lam bai vao tep s00_bailam.py, chay Run de co ketqua tatca la 1
 02a tao github repo, mo trinhduyetweb de kiemtra tep s00_bailam.py, va lay diachi/url aka githubbailamurl
 
 02b dán diachi githubbailamurl theo mẫu ở trang web duoiday
-    https://forms.gle/TODO
+    https://forms.gle/mb6ZrFw4pXW8DqeBA
 
 --- debai / problem
-Viet ham
-def hi(name):
-  # tra ve cau chao 'Hi {name}!'
+Viết hàm 
+  greeting(hour_str)
+giúp chat bot in ra câu chào theo buổi sáng/chiều/tối trong ngày 
+00:00AM  - 11:59AM Good morning!
+12:00PM  - 05:59PM Good afternoon!
+06:00PM  - 11:59PM Good evening!
 
-Khi chạy với đầuvào / input            | Kếtquả đẩura / output  | Thứtự mẫuthử 
--------------------------------------- | ---------------------- | -----------
-hi('AI-BTX')                           | Hi AI-BTX!
-hi('HSU')                              | Hi HSU!
-hi(None)                               | Hi!
-hi('')                                 | Hi!
+Khi chạy với đầuvào / input  | Kếtquả đẩura / output  | Thứtự mẫuthử 
+---------------------------- | ---------------------- | -----------
+greeting(hour_str='6am')     | Good morning!          | 00
+
+# AM / PM format
+greeting('6am')              | Good morning!          | 01
+greeting('6 am')             | Good morning!          | 02
+greeting('6AM')              | Good morning!          | 03
+greeting('6 AM')             | Good morning!          | 04
+
+greeting('9pm')              | Good evening!          | 05
+greeting('0900pm')           | Good evening!          | 06
+greeting('09:00pm')          | Good evening!          | 07
+greeting('09:00 pm')         | Good evening!          | 08
+greeting('09:00 PM')         | Good evening!          | 09
+
+greeting('1pm')              | Good afternoon!        | 10
+
+# 24-hours format
+greeting('06:00')            | Good morning!          | 11
+greeting('0600')             | Good morning!          | 12
+greeting('21:00')            | Good evening!          | 13
+greeting('2100')             | Good evening!          | 14
+
 """
 #endregion debai
 
 #region bailam
-def hi(name):
-  return 'TODO'
+def greeting(hour_str):
+  return 'todo'
+  
 
 if __name__=='__main__':
-  print( hi(name='AI-BTX') )
-  print( hi('HSU') )
-  print( hi(None) )
-  print( hi('') )
+  pass
 #endregion bailam
